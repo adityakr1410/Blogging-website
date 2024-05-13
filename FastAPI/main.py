86ea6900cdbbcbc829b96eb84a97f7e3fc8dc632
Aditya Kumar <92170363+adityakr1410@users.noreply.github.com>
2024-05-14 02:15:16 +0530

    disabled entry_root
    it was used at the start of the builing api to check connection

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request
-from routes.entry import entry_root
+# from routes.entry import entry_root
 from routes.blog import blog_root
 from routes.user import user_root
 from routes.comment import comment_root
@@ -9,7 +9,7 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
-app.include_router(entry_root)
+# app.include_router(entry_root)
 app.include_router(blog_root)
 app.include_router(user_root)
 app.include_router(comment_root)
